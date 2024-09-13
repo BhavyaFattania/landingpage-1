684bedd363def4b5eacfb1aa65402cae931cfded
Bhavya <147738039+BhavyaFattania@users.noreply.github.com>
2024-09-13 23:08:17 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function scrollToTop(){
+var timerHandle = setInterval(function() {
+  if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0)
+  window.scrollBy(0,-50); else clearInterval(timerHandle); },10);
+}
